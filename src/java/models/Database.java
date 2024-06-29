cd7c124d0b748b181190393250ac9c62f098061d
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2024-06-29 20:52:13 +0700

    init

---
+++ src/java/models/Database.java
@@ -0,0 +1,28 @@
+
+package models;
+
+import java.sql.*;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Database implements DatabaseInfo{
+    public static Connection getConnect(){
+        try{ 
+            Class.forName(DRIVERNAME); 
+	} catch(ClassNotFoundException e) {
+            System.out.println("Error loading driver" + e);
+	}
+        try{            
+            Connection con = DriverManager.getConnection(DBURL,USERDB,PASSDB);
+            return con;
+        }
+        catch(SQLException e) {
+            System.out.println("Error: " + e);
+        }
+        return null;
+    }
+}
---
+++ src/java/models/DatabaseInfo.java
@@ -0,0 +1,10 @@
+package models;
+
+
+public interface DatabaseInfo {
+    public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String DBURL="jdbc:sqlserver://CRISWORIDVN;databaseName=fpt_food;encrypt=false;trustServerCertificate=false;loginTimeout=30;";
+    public static String USERDB="sa";
+    public static String PASSDB="123";
+   
+}
